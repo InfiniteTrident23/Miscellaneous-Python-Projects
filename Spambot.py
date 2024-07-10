0aa437de2ede101e318d43421c1d683c78b19213
InfiniteTrident23 <128295541+InfiniteTrident23@users.noreply.github.com>
2024-07-10 15:43:57 +0530

    Update Spambot.py

--- Spambot.py
+++ Spambot.py
@@ -1,5 +1,8 @@
 # Fun code to annoy your friends!
 # Requires pyautogui library installation.
+# After entering the spam string, take your cursor to the desired location
+# WARNING: The program will not move the cursor, please shift it within 10 secs of the entering string!"
+
 import time, pyautogui
 
 a = int(input("How many times do you wanna spam? "))
